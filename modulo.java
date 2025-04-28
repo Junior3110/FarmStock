ea6db2dee3d10de54d6afca01ec3e6206c87456c
César Díaz <cesardiazq10@gmail.com>
2025-04-28 15:40:04 -0500

    Update modulo.java

--- modulo.java
+++ modulo.java
@@ -1,5 +1,10 @@
+
+Import Scanner.util.Scanner;
+
 public  class modulo {
     public static void main(String[] args) {
         System.out.println("hola mundo");
+        Scanner leer = new scanner(system.in);
+        int edad = 19;
     }
 }
