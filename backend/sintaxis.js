e6c65292cf1ebd1d148294f73df70f948d158c35
Cesar Diaz <cesar.diaz@example.com>
2025-07-08 16:29:52 -0500

    subo sintaxis de javaScript

---
+++ backend/sintaxis.js
@@ -0,0 +1,29 @@
+/*var nombre = "cesar";// var se usa para declarar variables globales la puedo usar en cualquier parte del codigo 
+
+let apellido = "Diaz"; // variable de alcance local solo puede ser usada dentro del bloque donde fue declarada y le puedo cambiar su valor
+
+apellido = "garcia"; // esta es la diferencia con const ya que con esta no se puede cambiar su valor
+
+{
+    let apellido = "diaz";
+}
+
+const edad = 9; // siempre va a valer lo mismo por eso es usado para cosas constantes como el numero pi
+  // no se puede cambiar el valor de una constante osea const
+
+{ 
+    const edad = 12; // 
+}
+
+console.log("Hola desde sintaxis.js");
+
+METODO MATH
+
+let numeroRamdom = Math.random();
+console.log(numeroRamdom);
+
+*/ 
+
+let nombre = "junior"; 
+let apellido = "diaz"; 
+console.log(nombre + " " + apellido); 
