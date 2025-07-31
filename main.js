b2ab9cbb887ad9c7ed82f8ca9758a9740015c475
Cesar Diaz <cesar.diaz@example.com>
2025-07-31 09:07:30 -0500

    carpetas

--- main.js
+++ main.js
@@ -1,31 +1,21 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, screen } = require('electron');
 const path = require('path');
 
 function createWindow() {
+  const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+
   const win = new BrowserWindow({
-    width: 1320,
-    height: 900,
+    width: Math.min(1320, width),
+    height: Math.min(900, height),
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false
     }
   });
+
+  win.loadFile(path.join(__dirname, 'app', 'registro-salida.html'));
+  win.center();
   win.webContents.openDevTools();
-  win.loadFile(path.join(__dirname, 'app', 'index.html'));
 }
 
 app.whenReady().then(createWindow);
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
-
-app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-    // fea 
-  }
-});
-
