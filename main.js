4ff6f6e9105b61035c584bd84c5a0edae48b7341
Cesar Diaz <cesar.diaz@example.com>
2025-08-04 07:03:36 -0500

    conexion entre paneles

--- main.js
+++ main.js
@@ -1,28 +1,28 @@
-const { app, BrowserWindow, screen } = require('electron');
+const { app, BrowserWindow, screen, ipcMain } = require('electron');
 const path = require('path');
+
+let win;
 
 function createWindow() {
   const { width, height } = screen.getPrimaryDisplay().workAreaSize;
 
-  const win = new BrowserWindow({
+  win = new BrowserWindow({
     width: Math.min(1320, width),
     height: Math.min(900, height),
-    show: false,
-   webPreferences: {
-  preload: path.join(__dirname, 'preload.js')
-}
-
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false // Solo así puedes usar `require` en el HTML
+    }
   });
 
-  win.loadFile(path.join(__dirname, 'app/HTMl', 'login.html'));
-
-  win.once('ready-to-show', () => {
-    win.show();
-    win.focus(); // Esto asegura que puedas escribir inmediatamente
-  });
-
+  win.loadFile(path.join(__dirname, 'app/HTML/login.html'));
   win.center();
   win.webContents.openDevTools();
 }
 
 app.whenReady().then(createWindow);
+
+// Escucha el mensaje desde el renderer
+ipcMain.on('abrir-login', () => {
+  win.loadFile(path.join(__dirname, 'app/HTML/registro-persona.html'));
+});
