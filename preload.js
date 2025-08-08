afd8cc92c74ac6c83d481f4b84ce9bcea8dab5ca
Cesar Diaz <cesar.diaz@example.com>
2025-08-08 11:01:15 -0500

    arreglo de calendario

--- preload.js
+++ preload.js
@@ -1,4 +1,9 @@
 // preload.js
-window.addEventListener('DOMContentLoaded', () => {
-  // Aquí puedes exponer funciones si necesitas luego
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('navegacion', {
+  irARegistro: () => ipcRenderer.send('ir-a-registro'),
+  irALogin: () => ipcRenderer.send('ir-a-login'),
+  irAInventario: () => ipcRenderer.send('ir-a-inventario'),
+  irAEstadisticas: () => ipcRenderer.send('ir-a-estats'),
 });
